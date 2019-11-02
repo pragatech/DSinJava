27fa68c0be0504b58cae02fe49eddf8a7a9fd5bd
pragatech <pragatheeswaran.a.c@outlook.in>
2019-11-02 14:06:52 +0530

    stack using array

---
+++ src/com/praga/logical/stack/StackUsingArray.java
@@ -0,0 +1,45 @@
+package com.praga.logical.stack;
+
+import java.lang.reflect.Array;
+import java.util.logging.Logger;
+
+public class StackUsingArray<E> {
+	private final static Logger log = Logger.getLogger(StackUsingArray.class.getName());
+	private E[] stack;
+	private int top;
+	private int size;
+	
+	public StackUsingArray(Class E, int size){
+		this.size = size;
+		stack = (E[]) Array.newInstance(E, size);
+		top = -1;
+	}
+	
+	public boolean isEmpty() {
+		return top == -1;
+	}
+	
+	public boolean isFull() {
+		return top == size -1;
+	}
+	
+	public void push(E data){
+		if(top == size-1) {
+			log.info("Stack is full");
+			return;
+		}
+		stack[++top] = data;
+		log.info(data +" is pushed");
+	}
+	
+	public E pop(){
+		if(top == -1) {
+			log.info("Stack is empty");
+			return null;
+		}
+		E data = stack[top];
+		top--;
+		log.info(data +" is poped");
+		return data;
+	}
+}
---
+++ src/com/praga/logical/stack/test/StackTestUsingArray.java
@@ -0,0 +1,28 @@
+package com.praga.logical.stack.test;
+
+import com.praga.logical.stack.StackUsingArray;
+
+public class StackTestUsingArray {
+	public static void main(String[] args) throws Exception {
+		StackUsingArray<Integer> s = new StackUsingArray(Integer.class, 5);
+		
+		System.out.println("Is stack empty?"+s.isEmpty());
+		s.push(2);
+		s.push(5);
+		s.push(6);
+		s.push(1);
+		s.push(3);
+		s.push(30);
+		System.out.println("Is stack Full?"+s.isFull());
+		s.pop();
+		s.pop();
+		s.pop();
+		System.out.println("Is stack empty?"+s.isEmpty());
+		s.pop();
+		s.pop();
+		s.pop();
+		s.push(30);
+		s.pop();
+		System.out.println("Is stack empty?"+s.isEmpty());
+	}
+}
